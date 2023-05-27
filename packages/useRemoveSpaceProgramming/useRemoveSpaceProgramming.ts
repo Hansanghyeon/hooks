0600491833eaa81b097eec9f577754eb159577cf
Hansanghyeon <hyeon@imweb.me>
2023-05-28 03:10:32 +0900

    chore: remove comment

--- packages/useRemoveSpaceProgramming/useRemoveSpaceProgramming.ts
+++ packages/useRemoveSpaceProgramming/useRemoveSpaceProgramming.ts
@@ -4,19 +4,6 @@ import { Ord } from 'fp-ts/string'
 import { camelCase } from 'lodash'
 import { useCallback, useState } from 'react'
 
-
-
-// 모든 value의 Key를 카멜케이스로 변환하는 함수
-// const camelizeKeys = <K extends Key>(value: { [key in K]: unknown }): { [key in K]: unknown } => {
-//   const result: { [key in K]: unknown } = {} as { [key in K]: unknown }
-//   for (const key in value) {
-//     if (Object.prototype.hasOwnProperty.call(value, key)) {
-//       result[camelCase(key)] = value[key]
-//     }
-//   }
-//   return result
-// }
-
 type RecordObject = Record<string, unknown>;
 
 function transformObjectKeys<T>(
